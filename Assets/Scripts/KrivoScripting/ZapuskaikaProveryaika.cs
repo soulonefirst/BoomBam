ce088fe6778d53fc6c33bcec2fe0da9bf412d728
fargusconner <fargusconner@yandex.ru>
2020-07-12 03:45:43 +0300

    изменил шарики

--- Assets/Scripts/KrivoScripting/ZapuskaikaProveryaika.cs
+++ Assets/Scripts/KrivoScripting/ZapuskaikaProveryaika.cs
@@ -6,6 +6,12 @@ public class ZapuskaikaProveryaika : MonoBehaviour
 {
     VisualEffect enemy;
     public bool hit;
+    private float ssmultAdd = 0;
+    private float ssrAdd = 0;
+    private float ssminAdd = 0;
+    private float ssmaxAdd = 0;
+    private float ssAdd = 0;
+    private float wfsAdd = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -26,11 +32,21 @@ public class ZapuskaikaProveryaika : MonoBehaviour
     IEnumerator Pulse()
     {
         enemy.SendEvent("Strike");
-        enemy.SetFloat("SparksSizeMultiplier", enemy.GetFloat("SparksSizeMultiplier") + 2);
-        enemy.SetFloat("SparksSpawnRate", enemy.GetFloat("SparksSpawnRate") + 8000);
-        yield return new WaitForSeconds(0.13f);
-        enemy.SetFloat("SparksSizeMultiplier", enemy.GetFloat("SparksSizeMultiplier") -1.9f);
+        enemy.SetFloat("SparksSizeMultiplier", enemy.GetFloat("SparksSizeMultiplier") + 2.5f + ssmultAdd);
+        enemy.SetFloat("TrailsSphereSizeMult", enemy.GetFloat("TrailsSphereSizeMult") + 0.7f);
+        enemy.SetFloat("SparksSpawnRate", enemy.GetFloat("SparksSpawnRate") + 700 + ssrAdd);
+        enemy.SetFloat("SparksSizeMinimum", enemy.GetFloat("SparksSizeMinimum") + 0.015f + ssminAdd);
+        enemy.SetFloat("SparksSizeMaximum", enemy.GetFloat("SparksSizeMaximum") + 0.02f + ssmaxAdd);
+        enemy.SetFloat("StripSize", enemy.GetFloat("StripSize") + 0.7f + ssAdd); 
+         yield return new WaitForSeconds(0.2f + wfsAdd);
+        enemy.SetFloat("SparksSizeMultiplier", enemy.GetFloat("SparksSizeMultiplier") - 2.4f - ssmultAdd);
+        enemy.SetFloat("TrailsSphereSizeMult", enemy.GetFloat("TrailsSphereSizeMult") - 0.35f);
 
-
-    }
+        ssmultAdd = 0.5f;
+        ssrAdd = 6500;
+        ssminAdd = 0.01f;
+        ssmaxAdd = 0.01f;
+        ssAdd = 1f;
+        wfsAdd = 0.05f;
+}
 }
