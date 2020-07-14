56713ae83b0ea3fa294e0d91bfdf8e43e8115a71
soulonefirst <toorbonoob@gmail.com>
2020-07-14 17:40:57 +0300

    Pool

---
+++ Assets/Scripts/System/SpawnVfxSystem.cs
@@ -0,0 +1,22 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+public class SpawnVfxSystem : SystemBase
+{
+    protected override void OnCreate()
+    {
+        SpawningVFX.Initialization();
+    }
+    protected override void OnUpdate()
+    {  
+        Entities
+            .WithoutBurst()
+            .ForEach((Entity entity, ref IdVfxData idVfx) => {
+            if (idVfx.Value >= 0)
+            {
+                SpawningVFX.VFXSpawn(entity, idVfx.Value);
+                idVfx.Value = -1;
+            }
+        }).Run();
+    }
+
+}
